52c9e87a380adb032000d3f5a9951897f00db421
Rudo <knipmess@gmail.com>
2024-11-05 14:13:00 +0200

    reverted to localhost

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -17,7 +17,7 @@ export default function ContactForm() {
       localStorage.setItem("subject", subject);
       localStorage.setItem("message", message);
       axios
-        .post("../api/server/send_email", {
+        .post("http://localhost:5000/send_email", {
           recipient_email: recipient_email,
           subject: subject,
           message: message,
