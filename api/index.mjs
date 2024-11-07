250fc3aef590fc8c4400e0c9c577baa66e3dfb32
Rudo <knipmess@gmail.com>
2024-11-07 08:16:52 +0200

    resend added to index.mjs

--- api/index.mjs
+++ api/index.mjs
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendResend() {
   const { data, error } = await resend.emails.send({
